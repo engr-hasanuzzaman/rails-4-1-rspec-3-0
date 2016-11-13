670b69e10d3d8778824d2f49afcb3380da89c4b3
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2016-11-13 21:29:15 +0600

    Update selenium driver - fail to run test with js: true for firefox and selenium version compatibility

---
+++ spec/features/about_modal_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'About BigCo modal' do
+  scenario 'toggles display of the modal about display' do
+  visit root_path
+
+  # save_and_open_page
+  expect(page).not_to have_content 'About BigCo, Inc.'
+  expect(page).not_to \
+  have_content 'BigCo produces the finest widgets in all the land'
+
+  click_link 'About Us'
+
+  expect(page).to have_content 'About BigCo'
+  expect(page).to \
+  have_content 'BigCo produces the finest widgets in all the land'
+
+  within '#about_us' do
+    click_button 'Close'
+  end
+
+
+  # expect(page).not_to have_content 'About BigCo'
+  # expect(page).not_to \
+  # have_content 'BigCo produces the finest widgets in all the land'
+  end
+end
